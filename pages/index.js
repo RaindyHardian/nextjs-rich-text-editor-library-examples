f664d6fc1cbcd8cc098092e82ce65722641d4c95
Raindy Hardiansyah <raindyhardian@gmail.com>
2022-03-13 22:27:39 +0700

    feat: init repo

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>NextJS Rich Text Editor Collections</title>
+        <meta
+          name="description"
+          content="Collection of NextJs Rich Text Editor Library on the internet"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
