9a5702e37cb489f875eeaf1d5dca06bd8da15dab
Raindy Hardiansyah <raindyhardian@gmail.com>
2022-03-13 23:43:54 +0700

    feat: add route

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
+    <div className='container'>
       <Head>
         <title>NextJS Rich Text Editor Collections</title>
         <meta
@@ -11,6 +12,28 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/react-quill">
+            <a>/react-quill</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/suneditor">
+            <a>/suneditor</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/react-draft-wysiwyg">
+            <a>/react-draft-wysiwyg</a>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
